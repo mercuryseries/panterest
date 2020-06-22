3cc2ddecb24906b381721bf8e266073a18f82e85
Honore Hounwanou <mercuryseries@gmail.com>
2020-06-22 04:30:07 -0400

    Make use of u filter and KnpTimeBundle ago filter

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Fidry\PsyshBundle\PsyshBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
 ];
