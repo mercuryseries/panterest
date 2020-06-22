a3069ef3376c51e368d6d9922e35a23d2574c6ec
Honore Hounwanou <mercuryseries@gmail.com>
2020-06-22 04:08:51 -0400

    Update delete path

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -78,7 +78,7 @@ class PinsController extends AbstractController
     }
 
     /**
-     * @Route("/pins/{id<[0-9]+>}/delete", name="app_pins_delete", methods={"DELETE"})
+     * @Route("/pins/{id<[0-9]+>}", name="app_pins_delete", methods={"DELETE"})
      */
     public function delete(Request $request, Pin $pin, EntityManagerInterface $em): Response
     {
