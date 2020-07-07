d7acd073ee6c558fc27c1ab8f2e0aaac504d1c52
Honore Hounwanou <mercuryseries@gmail.com>
2020-07-07 13:07:42 -0400

    Make of use webpack encore and add lang attribute to html tag

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Fidry\PsyshBundle\PsyshBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
