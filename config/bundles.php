736df15d1cffd8eac44424a33fb716f4b9a747d0
Honore Hounwanou <mercuryseries@gmail.com>
2020-07-15 19:23:54 -0400

    Add Liip imagine bundle for thumbnails generation

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -18,6 +18,7 @@ class PinType extends AbstractType
                 'required' => false,
                 'allow_delete' => true,
                 'download_uri' => false,
+                'imagine_pattern' => 'squared_thumbnail_small'
             ])
             ->add('title')
             ->add('description')
