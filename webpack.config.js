c3332acdadfaf693fd7aa10a9d89e9356b59403e
Honore Hounwanou <mercuryseries@gmail.com>
2020-07-15 20:12:49 -0400

    Copy files from assets/images to public/build via Encore and style homepage

--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,12 @@ Encore
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
     //.addEntry('admin', './assets/js/admin.js')
+    
+    .copyFiles({
+        from: './assets/images',
+        to: '[path][name].[hash:8].[ext]',
+        context: './assets'
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
