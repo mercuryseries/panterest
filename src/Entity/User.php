4887855097c0dafde48000d099de3d4fe8a74a5e
Honore Hounwanou <mercuryseries@gmail.com>
2020-09-08 04:44:15 -0400

    Little refactoring, use string instead of array for email uniqueentity assertion

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -15,7 +15,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(name="users")
  * @ORM\HasLifecycleCallbacks
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields="email", message="There is already an account with this email")
  */
 class User implements UserInterface
 {
