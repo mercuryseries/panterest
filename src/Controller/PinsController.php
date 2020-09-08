b960dabf0d2aebcd6ac62fc6a802caf37a590297
Honore Hounwanou <mercuryseries@gmail.com>
2020-09-07 21:22:09 -0400

    Associate new pin to authenticated user

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -36,8 +36,7 @@ class PinsController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $janeDoe = $userRepo->findOneBy(['email' => 'janedoe@example.com']);
-            $pin->setUser($janeDoe);
+            $pin->setUser($this->getUser());
             $em->persist($pin);
             $em->flush();
 
