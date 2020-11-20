43723a5b473f02ce4b3384c85f6c10b914d1e18c
Honore Hounwanou <mercuryseries@gmail.com>
2020-11-20 16:59:59 -0500

    Add reset password feature

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
