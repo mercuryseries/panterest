9b594c9b1e68401bc17ce6530c5a715a93c1f28e
Honore Hounwanou <mercuryseries@gmail.com>
2020-11-21 00:20:42 -0500

    Add user profile page

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccountController extends AbstractController
+{
+    /**
+     * @Route("/account", name="app_account")
+     */
+    public function show(): Response
+    {
+        return $this->render('account/show.html.twig');
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -211,6 +211,11 @@ class User implements UserInterface
         return $this->getFirstName() . ' ' . $this->getLastName();
     }
 
+    public function gravatar(?int $size = 100)
+    {
+        return sprintf('https://www.gravatar.com/avatar/%s/?s=%d', md5(strtolower(trim($this->getEmail()))), $size);
+    }
+
     public function isVerified(): bool
     {
         return $this->isVerified;
